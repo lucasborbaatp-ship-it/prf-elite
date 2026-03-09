b5c2de9deab17b779ee47534643df2f5d696eeae
lucasborbaatp-ship-it <lucasborbaatp@gmail.com>
2026-03-09 13:29:33 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,51 +1,45 @@
 
-let dados = JSON.parse(localStorage.getItem("dadosPRF")) || []
 
-function atualizarDashboard(){
-let totalQ = 0
-let totalA = 0
+let dados = JSON.parse(localStorage.getItem("dados")) || []
 
-dados.forEach(d=>{
-totalQ += d.q
-totalA += d.a
-})
+let ctx = document.getElementById("grafico").getContext("2d")
 
-let media = totalQ ? (totalA/totalQ)*100 : 0
-let nota = (media/100)*120
-
-document.getElementById("media").innerText = media.toFixed(1) + "%"
-document.getElementById("nota").innerText = nota.toFixed(1)
+let grafico = new Chart(ctx, {
+type:"line",
+data:{
+labels:[],
+datasets:[{
+label:"Aproveitamento (%)",
+data:[]
+}]
 }
+})
 
 function registrar(){
 
-let disciplina = document.getElementById("disciplina").value
 let q = Number(document.getElementById("questoes").value)
 let a = Number(document.getElementById("acertos").value)
 let e = Number(document.getElementById("erros").value)
 
-let cebraspe = a - e
+let perc = (a/q)*100
 
-dados.push({disciplina,q,a,e,cebraspe})
+dados.push(perc)
 
-localStorage.setItem("dadosPRF",JSON.stringify(dados))
+localStorage.setItem("dados",JSON.stringify(dados))
 
-document.getElementById("resultado").innerHTML =
-"Pontuação CEBRASPE: "+cebraspe
+grafico.data.labels = dados.map((_,i)=>i+1)
+grafico.data.datasets[0].data = dados
+grafico.update()
 
-atualizarDashboard()
 }
 
-function taf(){
+function salvarFlash(){
 
-let corrida = Number(document.getElementById("corrida").value)
-let barra = Number(document.getElementById("barra").value)
+let texto = document.getElementById("flashcard").value
+localStorage.setItem("flash",texto)
 
-let corridaStatus = corrida >= 2400 ? "OK" : "Treinar"
-let barraStatus = barra >= 5 ? "OK" : "Treinar"
-
-document.getElementById("tafResultado").innerHTML =
-"Corrida: "+corridaStatus+"<br>Barra: "+barraStatus
 }
 
-atualizarDashboard()
+document.getElementById("flashcard").value =
+localStorage.getItem("flash") || ""
+
